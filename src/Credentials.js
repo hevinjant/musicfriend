224d02d35fb42e1e016411820ef5f1b589844f03
Hevin Jant <hevin.jant@gmail.com>
2024-02-06 00:13:59 -0500

    moved Credentials.js

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+export const SPOTIFY_MUSICFRIEND_CLIENT_ID = "4cb59fcbc79e4bbcb51d908f87698410";
+
+export const FIREBASE_API_KEY = "AIzaSyCnoUJPCrhshTd7UT7CSodMv0ILIQudn_I";
+export const FIREBASE_AUTH_DOMAIN = "musicfriend-c549b.firebaseapp.com";
+export const FIREBASE_PROJECT_ID = "musicfriend-c549b";
+export const FIREBASE_STORAGE_BUCKET = "musicfriend-c549b.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "350517233861";
+export const FIREBASE_APP_ID = "1:350517233861:web:8bb840e25022be42f248ac";
+export const FIREBASE_MEASUREMENT_ID = "G-9E82HZ4G59";
--- src/Firebase.js
+++ src/Firebase.js
@@ -8,7 +8,7 @@ import {
   FIREBASE_MESSAGING_SENDER_ID,
   FIREBASE_APP_ID,
   FIREBASE_MEASUREMENT_ID,
-} from "../Credentials";
+} from "./Credentials";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
